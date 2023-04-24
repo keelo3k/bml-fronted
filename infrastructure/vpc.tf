c2b330c0a88752d6136e766c7f65fdb232692ae3
keelo3k <keelo3k@gmail.com>
2023-04-24 16:25:58 -0400

    changes to pc file

--- infrastructure/vpc.tf
+++ infrastructure/vpc.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "bml" {
   cidr_block = var.cidr_vpc
   instance_tenancy = var.instance_tenancy
-  enable_dns_support   = var.enable_dns_support
-  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns
+  enable_dns_hostnames = var.enable_dns
   
   tags = {
   Name = "bml"
